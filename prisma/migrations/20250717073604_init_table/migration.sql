b1927a4d6e4481601f621807b470ba52c2f025dd
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-07-17 14:38:14 +0700

    add migration database with prisma

---
+++ prisma/migrations/20250717073604_init_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
