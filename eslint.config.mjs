842df97491f62b13377aad08ceb033865fe2832f
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-07-31 19:25:51 +0700

    ad eslintignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["node_modules", ".next", "public"],
+    ignores: ["node_modules", ".next", "public", "src/generated/prisma/**"],
   },
 ];
 
