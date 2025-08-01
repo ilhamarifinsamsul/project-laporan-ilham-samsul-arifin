9402ac58aeab5569564ae8813456010b0ff94582
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-08-01 09:01:16 +0700

    modified footer again

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   const currentYear: number = new Date().getFullYear();
   return (
-    <footer className="bg-gray-300 text-gray-700 py-4 fixed bottom-0 w-full">
+    <footer className="bg-gray-300 text-gray-700 py-4 fixed">
       <div className="container mx-auto text-center">
         <p>&copy; {currentYear} Laporan Bencana Tagana. All rights reserved.</p>
       </div>
